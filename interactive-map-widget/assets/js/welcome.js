dbf2ddaba70f87c57f0c98474866a3140d2ffc99
taxdoo2023 <134045858+taxdoo2023@users.noreply.github.com>
2026-02-17 12:24:59 +0200

    Del

--- interactive-map-widget/assets/js/welcome.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Check if the form was submitted
-    if (document.getElementById('nahiro-redirect-flag')) {
-        window.location.href = nahiroRedirectData.redirectUrl;
-    }
-});
